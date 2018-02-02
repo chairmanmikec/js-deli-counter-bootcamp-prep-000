eb69e382797c646b971e396cc1510cdee12e113c
chairmanmikec <michaelc1012@gmail.com>
2018-02-02 01:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
   return katzDeliLine.length
 }
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine>0){
+    return katzDeliLine[0]
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
