08525976ffb665b5ec0e70680d7b87bd7eb05323
chairmanmikec <michaelc1012@gmail.com>
2018-02-02 02:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ function takeANumber(katzDeliLine,name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine[0]}.`
+    return `Currently serving ${katzDeliLine[0]}.`;
   } else {
     return 'There is nobody waiting to be served!'
-  } katzDeliLine.shift()
-    return katzDeliLine
+  } return katzDeliLine.shift();
 }
 
 function currentLine(katzDeliLine){
   var newArray=[];
   if (katzDeliLine.length > 0){
     for (var i=0; i<katzDeliLine.length; i++){
-     newArray.push(`${i+1}. ${katzDeliLine[i]}`)
+     newArray.push(` ${i+1}. ${katzDeliLine[i]}`)
   }
   return `The line is currently: ${newArray}`
   } else {
