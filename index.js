c4668dc58860fabbcf04692dd409acf8aa173847
chairmanmikec <michaelc1012@gmail.com>
2018-02-02 01:50:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function nowServing(katzDeliLine){
   }
 }
 
+function currentLine(katzDeliLine){
+  for (i=0; i<katzDeliLine.length; i++){
+    return `${i+1}. katzDeliLine[i]`
+  }
+  if (katzDeliLine > 0){
+  return "The line is currently:"
+  }else{
+    return 'The line is currently empty.'
+  }
+}
