56c4173933837bdd7310f0adb3eef8e5a1b006a1
chairmanmikec <michaelc1012@gmail.com>
2018-02-02 01:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var katzDeliLine = [];
+
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
@@ -9,12 +11,4 @@ function nowServing(katzDeliLine){
   } else {
     return 'There is nobody waiting to be served!'
   }
-}
-
-function currentLine(katzDeliLine){
-  if (katzDeliLine>0){
-    for (i=0;i<katzDeliLine.length; i++ ){
-      return `
-    }
-  } else
 }
